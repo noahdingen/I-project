910ac0f85cc513ff5284addc0077d583a6d9c979
noah <N.Willemsen2@student.han.nl>
2018-04-24 13:42:29 +0200

    DB conectie

--- database_connectie.php
+++ database_connectie.php
@@ -1,26 +1,9 @@
 <?php
-$hostnaam = 'localhost';
-$databasenaam = 'EenmaalAndermaal';
-$gebruikersnaam = 'sa';
-$wachtwoord = 'dbrules';
 
-try {
-    $handler = new PDO("sqlsrv:Server=$hostnaam;Database=$databasenaam;
-  ConnectionPooling=0", $gebruikersnaam, $wachtwoord);
-    $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $error) {
-    echo 'Er ging eventjes iets mis in de database. <br>';
-    echo 'test.<br>';
-}
-
-?>
-
-<?php
-
-$hostname  = "(local)";	// naam van ibase_server_info
+$hostname  = "localhost";	// naam van ibase_server_info
 $dbname = "EenmaalAndermaal";    	// naam van database
 $username = "sa";      	// gebruikersnaam
-$pw = "Renotje41";
+$pw = "dbrules";
 
 try {
     $dbh = new PDO("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$pw");
