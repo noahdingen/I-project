d340cc588b22f5560daac6d73313e63905007bc3
Renotje6 <38280916+Renotje6@users.noreply.github.com>
2018-04-24 13:35:09 +0200

    db connectie Reno

--- database_connectie.php
+++ database_connectie.php
@@ -12,3 +12,22 @@ try {
     echo 'Er ging eventjes iets mis in de database. <br>';
     echo 'test.<br>';
 }
+
+?>
+
+<?php
+
+$hostname  = "(local)";	// naam van ibase_server_info
+$dbname = "EenmaalAndermaal";    	// naam van database
+$username = "sa";      	// gebruikersnaam
+$pw = "Renotje41";
+
+try {
+    $dbh = new PDO("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$pw");
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+    echo "Er ging iets mis met de database.<br>";
+    echo "De melding is {$e->getMessage()}<br><br>";
+}
+?>
