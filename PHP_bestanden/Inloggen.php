586d3a8d5c6c4e5e9128b618b7352e8fc5f44a65
noah <N.Willemsen2@student.han.nl>
2018-04-25 16:27:52 +0200

    Proberen om de error te krijgen te zien als je ww of gebruikersnaam niet goed hebt. Inloggen werkt wel

--- PHP_bestanden/Inloggen.php
+++ PHP_bestanden/Inloggen.php
@@ -1,36 +1,36 @@
 <?php
-include_once '../Database_verbinding/database_connectie.php';
+
 
 $gebruiker= '';
 $pagina= '../index.php';
-$paginaFout = '../inloggen.php';
+$paginaFout = '../login.php';
+$error = '';
 
 if (isset($_POST['aanmelden'])){
-
-    if(!empty($_POST['Gebruikersnaam']) && !empty($_POST['wachtwoord'])){
-
-        $gebruiker = $_POST['Gebruikersnaam'];
+    include_once '../Database_verbinding/database_connectie.php';
+    if(!empty($_POST['gebruikersnaam']) && !empty($_POST['wachtwoord'])){
+        $gebruiker = $_POST['gebruikersnaam'];
         $wachtwoord =$_POST['wachtwoord'];
 
-        $sql = "SELECT gebruikersnaam FROM Gebruiker WHERE gebruikersnaam = '$gebruiker' AND wachtwoord = '$wachtwoord'";
+        $sql = "SELECT gebruikersnaam FROM Gebruiker WHERE gebruikersnaam = ? AND wachtwoord = ?";
         $opdracht = $dbh->prepare($sql);
         $opdracht->execute(array($gebruiker, $wachtwoord));
 
-        if (isset( $opdracht->fetch()['inlognaam'])){
+        if (isset( $opdracht->fetch()['gebruikersnaam'])){
             session_start();
-            $_SESSION['gebruiker']=$gebruiker;
-//            $sqlInsert = "INSERT INTO ingelogde(Naam)
-//		  				  VALUES (?)";
-            $naam = $gebruiker;
-            $opdracht = $dbh->prepare($sql);
-            $opdracht->execute (array($gebruiker));
+            $_SESSION['gebruikersnaam']=$gebruiker;
             header("refresh:0; url=$pagina.?Gebruiker=$gebruiker");
         }
         else {
-            $fout = "<p>* Gebruikersnaam of wachtwoord klopt niet</p>";
-            echo $fout;
+            $error = "Wachtwoord en/of gebruikersnaam is verkeerd";
+            header("refresh: 3; Location: ../login.php");
+            echo $error;
         }
     }
+}
+
+if (isset($_SESSION['errors'])) {
+    header("Location: ../login.php");
 }
 
 ?>
--- login.php
+++ login.php
@@ -1,27 +1,28 @@
 <?php
-include_once 'Database_verbinding/database_connectie.php';
-
+    include_once 'header.php';
+    include_once 'Database_verbinding/database_connectie.php';
+    include_once 'PHP_bestanden/Inloggen.php';
 ?>
-<?php include 'header.php' ?>
     <title>Signin Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
     <!--<link href="assets/css/bootstrap.min.css" rel="stylesheet">-->
     <link href="assets/css/signin.css" rel="stylesheet">
+
     <!-- Custom styles for this template -->
 
 <body class="text-center">
 
-<form class="form-signin">
-
+<form class="form-signin" action="PHP_bestanden/Inloggen.php" method="post">
+    <?php echo $error; ?>
     <img class="mb-4" src="https://icon-icons.com/icons2/474/PNG/512/auction-hammer_46873.png" alt="" width="72" height="72">
     <h1 class="h3 mb-3 font-weight-normal">Hier inloggen</h1>
     <div class="gebruikersnaam">
         <label for="Gebruikersnaam" class="sr-only">Gebruikersnaam</label>
-        <input type="text" id="Gebruikersnaam" class="form-control" placeholder="Gebruikersnaam" required autofocus>
+        <input type="text" id="Gebruikersnaam" name="gebruikersnaam" class="form-control" placeholder="Gebruikersnaam" required autofocus>
     </div>
     <label for="inputPassword" class="sr-only">Wachtwoord</label>
-    <input type="password" id="wachtwoord" class="form-control" placeholder="wachtwoord" required>
+    <input type="password" id="inputPassword" name="wachtwoord" class="form-control" placeholder="wachtwoord" required>
 
     <div class="checkbox mb-3">
         <label>
@@ -29,7 +30,7 @@ include_once 'Database_verbinding/database_connectie.php';
         </label>
     </div>
 
-    <button class="btn btn-lg btn-primary btn-block" type="aanmelden">Inloggen</button>
+    <input type="submit" class="btn btn-lg btn-primary btn-block" name="aanmelden" value="Aanmelden">
     <p class="mt-5 mb-3 text-muted">&copy; EenmaalAndermaal 2018</p>
 </form>
 <script>window.jQuery || document.write('<script src="assets/js/jquery-slim.min.js"><\/script>')</script>
@@ -38,3 +39,8 @@ include_once 'Database_verbinding/database_connectie.php';
 </body>
 
 </html>
+
+
+
+
+
