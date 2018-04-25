e37e4ef02d63f84d4b204642ec27439b993cdfd1
noah <N.Willemsen2@student.han.nl>
2018-04-25 11:16:46 +0200

    Echt vanalles wat ik niet meer weet, sorry mark dat ik het niet meer wist wat ik heb gedaan...

---
+++ PHP_bestanden/Inloggen.php
@@ -0,0 +1,41 @@
+<?php
+include_once '../Database_verbinding/database_connectie.php';
+
+$gebruiker=$fout="";
+$pagina= './index.php';
+$paginaFout = './inloggen.php';
+
+if (isset($_POST['aanmelden'])){
+
+    if(!empty($_POST['Gebruikersnaam']) && !empty($_POST['wachtwoord'])){
+
+        $gebruiker = $_POST['Gebruikersnaam'];
+        $wachtwoord =$_POST['wachtwoord'];
+
+        $sql = "SELECT gebruikersnaam FROM Gebruiker WHERE gebruikersnaam = $gebruiker AND wachtwoord = $wachtwoord";
+        $opdracht = $dbh->prepare($sql);
+        $opdracht->execute(array($gebruiker, $wachtwoord));
+
+        if (isset( $opdracht->fetch()['inlognaam'])){
+            session_start();
+            $_SESSION['gebruiker']=$gebruiker;
+//            $sqlInsert = "INSERT INTO ingelogde(Naam)
+//		  				  VALUES (?)";
+            $naam = $gebruiker;
+            $opdracht = $dbh->prepare($sqlInsert);
+            $opdracht->execute (array($gebruiker));
+            header("refresh:0; url=$pagina.?Gebruiker=$gebruiker");
+        }
+        else {
+            $fout = "<p>* Gebruikersnaam of wachtwoord klopt niet</p>";
+            echo $fout;
+        }
+    }
+}
+
+?>
+
+
+
+
+
--- login.php
+++ login.php
@@ -1,24 +1,17 @@
 <?php
+include_once 'Database_verbinding/database_connectie.php';
 
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-
+<?php include 'header.php' ?>
     <title>Signin Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!--<link href="assets/css/bootstrap.min.css" rel="stylesheet">-->
     <link href="assets/css/signin.css" rel="stylesheet">
     <!-- Custom styles for this template -->
 
 <body class="text-center">
-<?php include 'header.php' ?>
+
 <form class="form-signin">
 
     <img class="mb-4" src="https://icon-icons.com/icons2/474/PNG/512/auction-hammer_46873.png" alt="" width="72" height="72">
@@ -28,15 +21,15 @@
         <input type="text" id="Gebruikersnaam" class="form-control" placeholder="Gebruikersnaam" required autofocus>
     </div>
     <label for="inputPassword" class="sr-only">Wachtwoord</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+    <input type="password" id="wachtwoord" class="form-control" placeholder="wachtwoord" required>
 
     <div class="checkbox mb-3">
         <label>
-            <input type="checkbox" value="remember-me"> Onthoud mij niet
+            <input type="checkbox" value="remember-me"> Onthoud mij
         </label>
     </div>
 
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Inloggen</button>
+    <button class="btn btn-lg btn-primary btn-block" type="aanmelden">Inloggen</button>
     <p class="mt-5 mb-3 text-muted">&copy; EenmaalAndermaal 2018</p>
 </form>
 <script>window.jQuery || document.write('<script src="assets/js/jquery-slim.min.js"><\/script>')</script>
