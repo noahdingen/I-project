c2d6177467475507c6020fda685b4a369bb60ba5
Mike2211Projects <M.vanIngen3@student.han.n>
2018-04-25 16:23:07 +0200

    Verkoper worden werkt

--- PHP_bestanden/verkoperworden.php
+++ PHP_bestanden/verkoperworden.php
@@ -7,8 +7,14 @@
  */
 require_once('../Database_verbinding/database_connectie.php');
 
-$_SESSION['user'] = $_POST['username'];
+//$_SESSION['user'] = $_POST['username'];
+$gebruiker = $_POST['Gebruiker'];
 $bank = $_POST['banknaam'];
 $banknummer = $_POST['bankrekeningnummer'];
 $controle = $_POST['controleoptienaam'];
-$creditnummer = $_POST['creditcardnummer'];
+$creditnummer = $_POST['rekeningnummer'];
+
+
+$sql = "INSERT INTO Verkoper  VALUES('$gebruiker','$bank','$banknummer','$controle','$creditnummer')";
+$query = $dbh->prepare($sql);
+$query->execute(array($gebruiker,$bank,$banknummer,$controle,$creditnummer));
--- verkoper.php
+++ verkoper.php
@@ -6,24 +6,27 @@
  * Time: 15:45
  */
 include_once('header.php');
-include_once('Database_verbinding/database_connectie.php');
+//include_once('Database_verbinding/database_connectie.php');
 ?>
 <body class="text-center">
-            <form action="PHP_bestanden/verkoperworden.php" method="post" class="form-signin" enctype="text/plain">
+            <form action="PHP_bestanden/verkoperworden.php" method="post" class="form-signin" >
             <link href="assets/css/signin.css" rel="stylesheet">
  <?php
-if (!isset($_SESSION['user'])) {
+/* if (!isset($_SESSION['user'])) {
 
     echo "<H1 class=\"text-center\">U bent niet ingelogd, <br> U wordt zo doorgestuurd!</H1></form>";
     header("Refresh: 2; URL=index.php");
 }
-else{
+else{ */
 
 ?>
     <title>Verkoper</title>
             <h1 class="h3 mb-3 font-weight-normal">Verkoper worden?</h1>
+                <div class="form-group">
+                    <input type="text" name="Gebruiker" id="Gebruiker" class="form-control input-sm" placeholder="Gebruiker">
+                </div>
             <div class="form-group">
-                <select>
+                <select Name='banknaam'>
                     <option value="">Kies uw bank</option>
                     <option value="Rabobank">Rabobank</option>
                     <option value="ING">ING</option>
@@ -34,7 +37,7 @@ else{
                 <input type="text" name="bankrekeningnummer" id="bankrekeningnummer" class="form-control input-sm" placeholder="bankrekeningnummer">
             </div>
             <div class="form-group">
-                <select>
+                <select Name='controleoptienaam'>
                     <option value="">Kies uw controleoptie</option>
                     <option value="Creditcard">Creditcard</option>
                     <option value="Post">Post</option>
@@ -44,5 +47,5 @@ else{
                 <input type="text" name="rekeningnummer" id="rekeningnummer" class="form-control input-sm" placeholder="rekeningnummer">
             </div>
             <input type="submit" value="Upgrade naar verkoper" class="btn btn-info btn-block">
-            <input type="reset" value="Gegevens verwijderen" class="btn btn-info btn-block">
-<?php } ?>
+    <input type="reset" value="Gegevens verwijderen" class="btn btn-info btn-block"></form>
+<?php  ?>
