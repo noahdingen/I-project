26007c57be354a72a7db56cfe89c2daf1f18cd64
noah <N.Willemsen2@student.han.nl>
2018-04-26 10:57:13 +0200

    HTML Profielpagina gereviewd

--- profielpagina.php
+++ profielpagina.php
@@ -1,36 +1,25 @@
 <?php include 'header.php' ?>
-
-<html>
-<head>
-    <title>Dit is mijn eerste homepage</title>
-
-    <link href="assets/css/styles.css" rel="stylesheet">
+    <title>Profiel</title>
     <link href="assets/css/profielpagina.css" rel="stylesheet">
-
-</head>
 <body>
-
 <div class="kolommen">
-
-<div class="persoons-gegevens">
-<p>Gebruikersnaam: </p>
-<p>E-mail adres: </p>
-<p>Voornaam: </p>
-<p>Achternaam: </p>
-<p>Geboortedatum: </p>
-<p>Woonplaats: </p>
-<p>Straatnaam: </p>
-<p>Postcode: </p>
-<p>Type account: </p>
-    <p>
-    <a href="verkoper.php">Upgraden naar verkoper</a>
-    </p>
-    <a href="verkoper.php">Gegevens bijwerken</a>
-</div>
-
+    <div class="persoons-gegevens">
+        <p>Gebruikersnaam: </p>
+        <p>E-mail adres: </p>
+        <p>Voornaam: </p>
+        <p>Achternaam: </p>
+        <p>Geboortedatum: </p>
+        <p>Woonplaats: </p>
+        <p>Straatnaam: </p>
+        <p>Postcode: </p>
+        <p>Type account: </p>
+        <p>
+        <a href="verkoper.php">Upgraden naar verkoper</a>
+        </p>
+        <a href="verkoper.php">Gegevens bijwerken</a>
+    </div>
     <div class="persoonlijke-veilingen">
-           <h1>Mijn lopende veilingen</h1>
-
+        <h1>Mijn lopende veilingen</h1>
         <div class="container">
             <!-- Example row of columns -->
             <div class="row">
@@ -50,12 +39,9 @@
                     <p><a class="btn btn-secondary" href="#" role="button">Zie details &raquo;</a></p>
                 </div>
             </div>
-
         </div>
     </div>
-
 </div>
-
 <script>window.jQuery || document.write('<script src="assets/js/jquery-slim.min.js"><\/script>')</script>
 <script src="assets/js/popper.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
