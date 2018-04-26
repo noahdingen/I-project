16ea1f2b9fe81e375b0db4b5c3605702d4e31346
noah <N.Willemsen2@student.han.nl>
2018-04-26 09:50:39 +0200

    Database verbinding vernieuwd

--- Database_verbinding/database_connectie.php
+++ Database_verbinding/database_connectie.php
@@ -1,16 +1,20 @@
 <?php
-
-$hostname  = "localhost";	// naam van ibase_server_info
-$dbname = "EenmaalAndermaal";    	// naam van database
-$username = "sa";      	// gebruikersnaam
-$pw = "dbrules";
-
-try {
-    $dbh = new PDO("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$pw");
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch(PDOException $e) {
-    echo "Er ging iets mis met de database.<br>";
-    echo "De melding is {$e->getMessage()}<br><br>";
+function verbindMetDatabase() {
+    $hostnaam = 'localhost';
+    $databasenaam = 'EenmaalAndermaal';
+    $gebruikersnaam = 'sa';
+    $wachtwoord = 'dbrules';
+    global $pdo;
+    $pdo = new PDO("sqlsrv:Server=$hostnaam; Database=$databasenaam; ConnectionPooling = 0", "$gebruikersnaam", "$wachtwoord");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
 }
+
+function valideerFormulierinput($data) {
+    strip_tags($data);
+    trim($data);
+
+    return $data;
+}
+
 ?>
