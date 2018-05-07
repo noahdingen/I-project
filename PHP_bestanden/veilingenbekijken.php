766c2d7b463b90bf95ce52e000a596a70758e7f3
dirk47a <D.Sanders2@student.han.nl>
2018-05-07 14:45:46 +0200

    Nieuwe veilingenpagina toegevoegd

---
+++ PHP_bestanden/veilingenbekijken.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dirk Sanders
+ * Date: 7-5-2018
+ * Time: 14:11
+ */
+
+include_once '../Database_verbinding/database_connectie.php';
+
+$conn = verbindMetDatabase();
+
+$data = $conn->prepare("SELECT * FROM Voorwerp");
+$data->execute();
+$resultaat = $data->fetchAll(PDO::FETCH_NAMED);
+echo "<div>";
+for($i = 0; $i < count($resultaat); $i++){
+    echo "<a href='veilingenbekijken.php?id=" . $resultaat[$i]['voorwerpnummer'] . "'>";
+    echo "<h1>" . $resultaat[$i]['voorwerpnummer'] . "</h1>";
+    echo "<div>" . $resultaat[$i] ['titel'] . "</div>";
+    echo "<div>" . $resultaat[$i] ['beschrijving'] . "</h1>";
+    echo "</a>";
+}
+
+echo "</div>";
+//
