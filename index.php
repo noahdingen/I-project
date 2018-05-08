db2a25347d6cf3c295d8944a2668c5ca224b45c7
Mark <MA.Ogink@student.han.nl>
2018-05-08 14:40:51 +0200

    bootstrap regeltje weggehaald

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ include 'header.php';
     <link href="assets/css/index.css" rel="stylesheet">
   <body>
     <main role="main">
-      <!-- Main jumbotron for a primary marketing message or call to action -->
       <div class="jumbotron">
         <div class="container">
           <h1 class="display-3">Geachte <?php echo $bezoeker[0]. $bezoeker[1];?></h1>
