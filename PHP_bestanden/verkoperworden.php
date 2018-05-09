9384af05d56745d51da108fc3c6d40bfdfabedc8
Mike2211Projects <M.vanIngen3@student.han.n>
2018-05-09 11:40:00 +0200

    WERKENDDD

--- PHP_bestanden/verkoperworden.php
+++ PHP_bestanden/verkoperworden.php
@@ -4,23 +4,23 @@ include_once('../Database_verbinding/database_connectie.php');
 session_start();
 //Regel hieronder is voor server!
 //require_once '../Server_verbinding/SQLSrvConnect.php';
-$_SESSION['user'] = $_POST['username'];
+
 $bank = $_POST['banknaam'];
 $banknummer = $_POST['bankrekeningnummer'];
 $controle = $_POST['controleoptienaam'];
 $creditnummer = $_POST['rekeningnummer'];
 $verkoper = 'wel';
-$gast = $_SESSION['user'];
+$gast = $_SESSION['gebruikers'];
 
 $dbh = verbindMetDatabase();
 
 
 
-$sql = "INSERT INTO Verkoper  VALUES('$gebruiker','$bank','$banknummer','$controle','$creditnummer')";
+$sql = "INSERT INTO Verkoper  VALUES('$gast','$bank','$banknummer','$controle','$creditnummer')";
 $query = $dbh->prepare($sql);
-$query->execute(array($gebruiker,$bank,$banknummer,$controle,$creditnummer));
+$query->execute(array($gast,$bank,$banknummer,$controle,$creditnummer));
 
-$sql2 = "UPDATE Gebruiker  set verkoper = '$verkoper' WHERE gebruikersnaam = $gast ";
+$sql2 = "UPDATE Gebruiker  set verkoper = '$verkoper' WHERE gebruikersnaam = '$gast' ";
 $query = $dbh->prepare($sql2);
 $query->execute();
 
