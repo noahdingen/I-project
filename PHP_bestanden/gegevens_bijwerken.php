42553c91a548f6b755371eb1620250a8fa832016
Mark <MA.Ogink@student.han.nl>
2018-05-09 15:59:30 +0200

    Gegevens_bijwerken weer toegevoegd

---
+++ PHP_bestanden/gegevens_bijwerken.php
@@ -0,0 +1,12 @@
+<?php
+
+include "../profielpagina.php";
+
+foreach ($klantgegevens as $value){
+    if(isset($value)){
+        echo
+    }
+}
+
+//header("location: ../profielpagina.php?bewerken=true");
+?>
