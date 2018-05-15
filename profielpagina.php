4a08cb1bb0038839d67c3ad7965ff207ad54a964
noah <N.Willemsen2@student.han.nl>
2018-05-15 14:08:28 +0200

    het beste van profielpagina gemaakt, miss bij een gaatje in de planning dit verberteren

--- profielpagina.php
+++ profielpagina.php
@@ -94,7 +94,7 @@ else {
 		<a href="verkoper.php">Upgraden naar verkoper</a>
 		</p>
 		<a href="profielpagina.php?bewerken=true">Gegevens bijwerken</a>'; }
-		
+
 }
 
 ?>
@@ -152,6 +152,15 @@ if($verkoper == 'ja  '){
         </div>
     </div>
 </div>
+</main>
+
+<footer class="container">
+    <p>&copy; EenmaalAndermaal 2018</p>
+</footer>
+
+<!-- Bootstrap core JavaScript
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="assets/js/jquery-slim.min.js"><\/script>')</script>
 <script src="assets/js/popper.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
