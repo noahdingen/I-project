50748f8e72d8fb3a3a0d2cb67b45b3f4330442d2
Mike2211Projects <M.vanIngen3@student.han.n>
2018-05-17 09:54:18 +0200

    update states

---
+++ php_bestanden/db_registratie.php
@@ -0,0 +1,99 @@
+<?php 
+session_start();
+// maak verbinding met database op localhost
+require_once '../Database_verbinding/database_connectie.php';
+//Regel hieronder is voor server!
+//require_once '../Server_verbinding/SQLSrvConnect.php';
+
+$gebruiker="";
+$pagina= './login.php';
+$pdo = verbindMetDatabase();
+//Regel hieronder is voor server!
+//$pdo = $conn;
+
+// is er op de knop aanmelden geklikt?
+if (isset($_POST['aanmelden'])){
+    // zijn de velden gebruiker en wachtwoord ingevuld?
+    if(!empty($_POST['gebruikersnaam']) && !empty($_POST['wachtwoord'])){
+		if($_POST['wachtwoord'] == $_POST['bevestig_wachtwoord']){
+			// de ingevoerde gegevens opslaan in variabelen
+			$gebruiker = $_POST['gebruikersnaam'];
+			$wachtwoord =$_POST['wachtwoord'];
+		    $wachtwoord = password_hash($wachtwoord, PASSWORD_BCRYPT);
+			$email = $_POST['email'];
+			$voornaam = $_POST['voornaam'];
+			$achternaam = $_POST['achternaam'];
+			$geboortedatum = $_POST['geboortedatum'];
+			$adres = $_POST['adres'];
+			$postcode = $_POST['postcode'];
+			$plaatsnaam = $_POST['plaatsnaam'];
+			$geheime_vraag = geheimeVraag();
+			$antwoord = $_POST['antwoord'];
+			$adresregel2 = $adres;
+			$landnaam = 'nederland';
+			$verkoper = 'nee';
+            $conn = verbindMetDatabase();
+
+            $data = $conn->prepare("SELECT verkoper FROM Gebruiker WHERE gebruikersnaam = '$gebruiker' ");
+            $data->execute();
+            $resultaat = $data->fetchAll(PDO::FETCH_NAMED);
+
+            if(count($resultaat)>0){
+                $error = "Gebruiksnaam bestaat al";
+                header("refresh:0; url='../registreren.php?error=$error'");
+            }
+
+            $subject = 'Bedankt voor uw registratie op IConcept';
+            $emailtekst = 'Dit is een verificatie mail om uw account te activeren op onze website:
+            http://iproject39.icasites.nl/login.php
+                        
+            Met vriendelijke groeten, IConcepts.
+            Deze mail is automatisch gegenereed.';
+            $to = $email;
+            $from = 'iconcepts39@gmail.com';
+
+            $headers   = array();
+            $headers[] = "MIME-Version: 1.0";
+            $headers[] = "Content-type: text/plain; charset=iso-8859-1";
+            $headers[] = "From: IConcepts <{$from}>";
+            $headers[] = "X-Mailer: PHP/".phpversion();
+
+            mail($to, $subject, $emailtekst, implode("\r\n", $headers), "-f".$from );
+            $sql = "INSERT INTO Gebruiker(gebruikersnaam, wachtwoord, voornaam, 
+                                                    achternaam, adresregel1, adresregel2, postcode, plaatsnaam, landnaam, datum, emailadres
+                                                    ,vraagnummer, antwoordtekst, verkoper) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $opdracht = $pdo->prepare($sql);
+            $opdracht->execute(array($gebruiker, $wachtwoord, $voornaam, $achternaam, $adres, $adresregel2, $postcode,
+                                                 $plaatsnaam, $landnaam, $geboortedatum, $email, $geheime_vraag, $antwoord, $verkoper));
+			header("refresh:0; url='../login.php'");
+		}
+    }
+	else{
+			$error = "Wachtwoorden komen niet overeen";
+			header("refresh:0; url='../registreren.php?error=$error'");
+        }
+}
+else{
+			header("refresh:0; url='../registreren.php'");
+}
+	
+	
+	
+	
+function geheimeVraag(){
+	$geheimeVraag = $_POST['geheime_vraag'];
+	
+	if($geheimeVraag == 'Waar ben je geboren?'){
+		return 1;
+	} else if($geheimeVraag == 'Wat is je lievelingsdier?'){
+		return 2;
+	} else {
+		return 3;
+	}
+}
+	
+	
+	
+	
+	
+?>
---
+++ php_bestanden/gegevens_bijwerken.php
@@ -0,0 +1,11 @@
+<?php
+
+
+foreach ($klantgegevens as $value){
+    if(isset($value)){
+        echo $value;
+    }
+}
+
+header("location: ../profielpagina.php?bewerken=false");
+?>
