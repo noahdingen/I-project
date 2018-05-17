b62a8af1b6c388af1f94c0ecb4160838d0f1be0f
Mark <MA.Ogink@student.han.nl>
2018-05-17 10:46:07 +0200

    wachtwoord wijzigen pagina toegevoegd

--- wachtwoord_wijzigigen.php
+++ wachtwoord_wijzigigen.php
@@ -10,23 +10,14 @@ include 'header.php'
 ?>
 <link href="assets/css/login.css" rel="stylesheet">
 <div class="container">
-    <form class="col-md-3 col-form-label" action="wachtwoord.php">
+    <form class="col-md-3 col-form-label" action="login.php">
         <img class="mb-4" src="https://icon-icons.com/icons2/474/PNG/512/auction-hammer_46873.png" alt="" width="72" height="72">
-        <p>Als U de geheime vraag correct beantwoordt, ontvangt U een e-mail met verdere instructies voor het veranderen van uw wachtwoord.</p>
+        <p>Wijzig uw wachtwoord</p>
         <div class="form-group">
-            <input id="gebruikersnaam" class="form-control" name="gebruikersnaam" type="text" placeholder="Gebruikersnaam" required>
+            <input type="password" class="form-control" name="wachtwoord" id="wachtwoord" placeholder="Wachtwoord" required>
         </div>
         <div class="form-group">
-            <label for="geheime_vraag">Geheime vraag</label>
-            <select class="form-control form-control-lg" id="geheime_vraag" name="geheime_vraag" required>
-                <option value="">...</option>
-                <option value="1">Waar ben je geboren?</option>
-                <option value="2">Wat is je lievelingsdier?</option>
-                <option value="3">Wie is jouw superheld?</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <input type="text" class="form-control" name="antwoord" id="antwoord" placeholder="Antwoord" required>
+            <input type="password" class="form-control" name="wachtwoord" id="wachtwoord" placeholder="Herhaal wachtwoord" required>
         </div>
         <div class="container">
             <button type="submit" class="btn btn-primary">Verzenden</button>
