8ce1dee4798b13007fc85a1e7a118c7a7773e825
Wilting18 <bob.wilting@live.nl>
2018-05-22 15:23:24 +0200

    'gegevens_bijwerken commentaar toegevoegd

--- php_bestanden/gegevens_bijwerken.php
+++ php_bestanden/gegevens_bijwerken.php
@@ -9,17 +9,19 @@ $oude_gebruikersnaam = $_GET['gebruikersnaam'];
 $nieuwe_gebruikersnaam = $_POST['gebruikersnaam'];
 foreach ($_POST as $key => $value){
     if($value != '') {
+        // Past gebruikersnaam aan als deze word ngevuld
         echo "UPDATE Gebruiker SET $key = '$value' WHERE gebruikersnaam = '$oude_gebruikersnaam'";
         if($key==='gebruikersnaam' && $nieuwe_gebruikersnaam!==''){
             $data = $pdo->prepare("UPDATE Gebruiker SET $key = '$value' WHERE gebruikersnaam = '$oude_gebruikersnaam'");
             $_SESSION['gebruikers'] = $nieuwe_gebruikersnaam;
         }
+        // Geen nieuwe gebruikersnaam opgegeven
         else if($nieuwe_gebruikersnaam===''){
             $data = $pdo->prepare("UPDATE Gebruiker SET $key = '$value' WHERE gebruikersnaam = '$oude_gebruikersnaam'");
         }
+        //Als er meerdere gegevens worden ingevuld
         else{
             $data = $pdo->prepare("UPDATE Gebruiker SET $key = '$value' WHERE gebruikersnaam = '$nieuwe_gebruikersnaam'");
-            $_SESSION['gebruikers'] = $nieuwe_gebruikersnaam;
         }
         $data->execute();
     }
