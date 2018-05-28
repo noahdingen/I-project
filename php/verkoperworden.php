84dbadc54c7130dbaba87d07a332324b1b883fc5
noah <N.Willemsen2@student.han.nl>
2018-05-28 16:23:26 +0200

    review dirks ding

--- php/verkoperworden.php
+++ php/verkoperworden.php
@@ -1,69 +1,63 @@
 <?php
 
 include_once('../databaseverbinding/database_connectie.php');
-session_start();
+if (!isset($_SESSION)) {
+    session_start();
+}
+
 //Regel hieronder is voor server!
 //require_once '../Server_verbinding/SQLSrvConnect.php';
 
 $bank = $_POST['banknaam'];
 $banknummer = $_POST['IBAN-Nummer'];
-var_dump($banknummer);
 $creditcardnummer = $_POST['creditcardnummer'];
-var_dump($creditcardnummer);
 $verkoper = 'ja  ';
 $gast = $_SESSION['gebruikers'];
 $controle = 'Creditcard';
-$dbh = verbindMetDatabase();
+$conn = verbindMetDatabase();
 $error = "Vul alle gegevens in";
 $error2 = "Vul een geldig IBAN-Nummer in";
 
 
 function checkIBAN($iban) {
 
-    // Normalize input (remove spaces and make upcase)
+    // maakt input normaal (haalt grote letters en spaties weg)
     $iban = strtoupper(str_replace(' ', '', $iban));
 
     if (preg_match('/^[A-Z]{2}[0-9]{2}[A-Z0-9]{1,30}$/', $iban)) {
-        $country = substr($iban, 0, 2);
+        $land = substr($iban, 0, 2);
         $check = intval(substr($iban, 2, 2));
         $account = substr($iban, 4);
 
-        // To numeric representation
-        $search = range('A','Z');
+        // naar nummers
+        $zoeken = range('A','Z');
         foreach (range(10,35) as $tmp)
-            $replace[]=strval($tmp);
-        $numstr=str_replace($search, $replace, $account.$country.'00');
+            $vervang[]=strval($tmp);
+        $numstr=str_replace($zoeken, $vervang, $account.$land.'00');
 
-        // Calculate checksum
-        $checksum = intval(substr($numstr, 0, 1));
+        // berekent het
+        $checksom = intval(substr($numstr, 0, 1));
         for ($pos = 1; $pos < strlen($numstr); $pos++) {
-            $checksum *= 10;
-            $checksum += intval(substr($numstr, $pos,1));
-            $checksum %= 97;
+            $checksom *= 10;
+            $checksom += intval(substr($numstr, $pos,1));
+            $checksom %= 97;
         }
 
-        return ((98-$checksum) == $check);
+        return ((98-$checksom) == $check);
     } else
         return false;
 }
 
-
     if (!checkIBAN($banknummer)) {
-        header("location: ../verkoper.php?error=$error2");
-    } else {
+        header("location: ../verkoper.php?error=$error");
+    }else{
+        $sql = "INSERT INTO Verkoper  VALUES($gast, $bank, $banknummer, $controle, $creditcardnummer)";
+        $query = $conn->prepare($sql);
+        $query->execute(array($gast, $bank, $banknummer, $controle, $creditcardnummer));
 
-        if (empty($banknummer) || empty($creditcardnummer) || empty($bank)) {
-            header("location: ../verkoper.php?error=$error");
-        } else {
-            $sql = "INSERT INTO Verkoper  VALUES('$gast','$bank','$banknummer','$controle','$creditcardnummer')";
-            $query = $dbh->prepare($sql);
-            $query->execute(array($gast, $bank, $banknummer, '$controle', $creditcardnummer));
+        $sql2 = "UPDATE Gebruiker  set verkoper = '$verkoper' WHERE gebruikersnaam = '$gast' ";
+        $query = $conn->prepare($sql2);
+        $query->execute();
 
-            $sql2 = "UPDATE Gebruiker  set verkoper = '$verkoper' WHERE gebruikersnaam = '$gast' ";
-            $query = $dbh->prepare($sql2);
-            $query->execute();
-
-            header("Location: ../profielpagina.php?bewerken=false");
-        }
-
+        header("Location: ../profielpagina.php?bewerken=false");
 }
