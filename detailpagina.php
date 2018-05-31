8c86b50be608fac10fe6e415d38ede812247aa13
dirk47a <D.Sanders2@student.han.nl>
2018-05-31 13:35:57 +0200

    ff die vakjes weer op de juiste plek gezet

--- detailpagina.php
+++ detailpagina.php
@@ -107,15 +107,17 @@ include 'header.php';
             <div class="bg-light text-center">
                 <?php haalverkoperop($voorwerpnummer); ?>
             </div>
-        </div>
-                <div class="col-md-5 my-4">
-                    <h1>
-                        Product informatie
-                    </h1>
-                    <div class="bg-light text-center">
-                        <?php haalvoorwerpdetailsop($voorwerpnummer); ?>
-                    </div>
+
+                <h1>
+                    Product informatie
+                </h1>
+                <div class="bg-light text-center">
+                    <?php haalvoorwerpdetailsop($voorwerpnummer); ?>
                 </div>
+            
+
+        </div>
+
         </div>
     </div>
 </main>
