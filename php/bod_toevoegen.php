8364b5f0ff1f0c7280f46f2df0eaad791365e4cf
noah <N.Willemsen2@student.han.nl>
2018-06-05 14:20:46 +0200

    error over tien mil bieden

--- php/bod_toevoegen.php
+++ php/bod_toevoegen.php
@@ -9,20 +9,20 @@ if($_SESSION['gebruikers'] == ''){
     $error = "U dient ingelogd te zijn om te kunnen bieden";
     header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&error=$error");
 }
-else{
-$voorwerpnummer = $_GET['voorwerpnummer'];
-$bodbedrag = $_POST['bodbedrag'];
-$gebruikersnaam = $_SESSION['gebruikers'];
-global $conn;
-$conn = new PDO("sqlsrv:Server=mssql.iproject.icasites.nl; Database=iproject39; ConnectionPooling = 0", "iproject39", "Mj9cP5NoYv");
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$pdo = $conn;
-$sql = $conn->prepare("SELECT startprijs FROM Voorwerp WHERE voorwerpnummer = ?");
-$sql->execute(array($voorwerpnummer));
-$sql_check = $pdo->prepare("SELECT MAX(bodbedrag) as hoogste_bod, gebruikersnaam FROM Bod WHERE voorwerpnummer = ? GROUP BY bodbedrag , gebruikersnaam ORDER BY bodbedrag DESC ");
-$sql_check->execute(array($voorwerpnummer));
-$hoogste_bod = $sql_check->fetchAll(PDO:: FETCH_ASSOC);
-$startbod = $sql->fetchAll();
+else {
+    $voorwerpnummer = $_GET['voorwerpnummer'];
+    $bodbedrag = $_POST['bodbedrag'];
+    $gebruikersnaam = $_SESSION['gebruikers'];
+    global $conn;
+    $conn = new PDO("sqlsrv:Server=mssql.iproject.icasites.nl; Database=iproject39; ConnectionPooling = 0", "iproject39", "Mj9cP5NoYv");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = $conn;
+    $sql = $conn->prepare("SELECT startprijs FROM Voorwerp WHERE voorwerpnummer = ?");
+    $sql->execute(array($voorwerpnummer));
+    $sql_check = $pdo->prepare("SELECT MAX(bodbedrag) as hoogste_bod, gebruikersnaam FROM Bod WHERE voorwerpnummer = ? GROUP BY bodbedrag , gebruikersnaam ORDER BY bodbedrag DESC ");
+    $sql_check->execute(array($voorwerpnummer));
+    $hoogste_bod = $sql_check->fetchAll(PDO:: FETCH_ASSOC);
+    $startbod = $sql->fetchAll();
 
     $check_tijd = $pdo->prepare("SELECT looptijdeindeDag, looptijdeindeTijdstip FROM voorwerp WHERE voorwerpnummer = ?");
     $check_tijd->execute(array($voorwerpnummer));
@@ -33,51 +33,53 @@ $startbod = $sql->fetchAll();
     $eind_tijdstip = $eind_tijd[0]['looptijdeindeTijdstip'];
     $huidige = date('Y-m-d H:i:s');
 
-    $eind = $eind_dag." ".$eind_tijdstip;
+    $eind = $eind_dag . " " . $eind_tijdstip;
 
-    if($huidige > $eind){
+    if ($huidige > $eind) {
         header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&veilingstatus=0");
-    }else{
-$state = $pdo->prepare("SELECT verkoper FROM voorwerp WHERE voorwerpnummer = ?");
-$state->execute(array($voorwerpnummer));
-$verkoper = $state->fetchAll();
-if($gebruikersnaam != $verkoper[0]['verkoper']){
-if (empty($hoogste_bod) && $hoogste_bod > $startbod[0]['startprijs']){
-    var_dump( $startbod[0]['startprijs']);
-    echo $hoogste_bod;
-    $sql_insert = $pdo->prepare("INSERT INTO Bod VALUES (?, ?, ?, CAST(GETDATE() AS DATE), convert(time,GETDATE()))");
-    $sql_insert->execute(array($voorwerpnummer, $bodbedrag, $gebruikersnaam));
-    $error = "";
-   header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer");
+    } else {
+        $state = $pdo->prepare("SELECT verkoper FROM voorwerp WHERE voorwerpnummer = ?");
+        $state->execute(array($voorwerpnummer));
+        $verkoper = $state->fetchAll();
+        if ($gebruikersnaam != $verkoper[0]['verkoper']) {
+            if ($bodbedrag < 10000000) {
+                if (empty($hoogste_bod) && $hoogste_bod > $startbod[0]['startprijs']) {
+                    var_dump($startbod[0]['startprijs']);
+                    echo $hoogste_bod;
+                    $sql_insert = $pdo->prepare("INSERT INTO Bod VALUES (?, ?, ?, CAST(GETDATE() AS DATE), convert(time,GETDATE()))");
+                    $sql_insert->execute(array($voorwerpnummer, $bodbedrag, $gebruikersnaam));
+                    $error = "";
+                    header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer");
+                } else {
+                    if ($hoogste_bod[0]["hoogste_bod"] < $bodbedrag && $bodbedrag > $startbod[0]['startprijs'] && $bodbedrag - $hoogste_bod[0]["hoogste_bod"] >= 1) {
+                        $lengte = sizeof($hoogste_bod);
+                        if ($hoogste_bod[0]["gebruikersnaam"] != $gebruikersnaam) {
+                            $sql_insert = $pdo->prepare("INSERT INTO Bod VALUES (?, ?, ?, CAST(GETDATE() AS DATE), convert(time,GETDATE()))");
+                            $sql_insert->execute(array($voorwerpnummer, $bodbedrag, $gebruikersnaam));
+                            $error = "";
+                            header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer");
+                        } else {
+                            $error = "U heeft al het hoogste bod geplaatst";
+                            header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&error=$error");
+                        }
+                    } else {
+                        if ($hoogste_bod[0]["hoogste_bod"] > $startbod[0]['startprijs']) {
+                            $error = 'bodbedrag is te laag!<br> U dient boven het startbedrag te bieden en boven het hoogste bod<br>U dient een minimale verhoging van €1,- te doen<br> dit is: €' . $hoogste_bod[0]["hoogste_bod"];
+                            header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&error=$error");
+                        } else {
+                            $error = 'bodbedrag is te laag!<br> U dient boven het startbedrag te bieden en boven het hoogste bod<br> dit is: €' . $startbod[0]["startprijs"];
+                            header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&error=$error");
+                        }
+                    }
+                }
+            }else {
+                $error = 'U kunt niet hoger dan €9.999.999,99 bieden';
+                header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&error=$error");
+            }
+        } else {
+            $error = 'U kunt niet op uw eigen veilingen bieden!';
+            header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&error=$error");
+        }
+    }
 }
-else{
-if($hoogste_bod[0]["hoogste_bod"]<$bodbedrag && $bodbedrag > $startbod[0]['startprijs'] && $bodbedrag - $hoogste_bod[0]["hoogste_bod"] >= 1 ) {
-    $lengte =  sizeof($hoogste_bod);
-    if($hoogste_bod[0]["gebruikersnaam"]!=$gebruikersnaam){
-        $sql_insert = $pdo->prepare("INSERT INTO Bod VALUES (?, ?, ?, CAST(GETDATE() AS DATE), convert(time,GETDATE()))");
-        $sql_insert->execute(array($voorwerpnummer, $bodbedrag, $gebruikersnaam));
-        $error = "";
-        header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer");
-    }
-    else {
-        $error = "U heeft al het hoogste bod geplaatst";
-        header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&error=$error");
-    }
-}
-else{
-    if($hoogste_bod[0]["hoogste_bod"] > $startbod[0]['startprijs']){
-        $error = 'bodbedrag is te laag!<br> U dient boven het startbedrag te bieden en boven het hoogste bod<br>U dient een minimale verhoging van €1,- te doen<br> dit is: €'. $hoogste_bod[0]["hoogste_bod"] ;
-        header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&error=$error");
-    }
-    else{
-        $error = 'bodbedrag is te laag!<br> U dient boven het startbedrag te bieden en boven het hoogste bod<br> dit is: €'. $startbod[0]["startprijs"] ;
-        header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&error=$error");
-    }
-}}}
-
-else{
-        $error = 'U kunt niet op uw eigen veilingen bieden!';
-        header("location: ../detailpagina.php?voorwerpnummer=$voorwerpnummer&error=$error");
-    }}}
-
 ?>
